316ada5d6fa3372e580f46185a7d249baaa2f388
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-11-14 09:26:27 +0700

    Update GameplayChangersSubstate.hx

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -63,7 +63,7 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 		var option:GameplayOption = new GameplayOption('Playback Rate', 'songspeed', 'float', 1);
 		option.scrollSpeed = 1;
 		option.minValue = 0.5;
-		option.maxValue = 3.0;
+		option.maxValue = 8.0; // im troller
 		option.changeValue = 0.05;
 		option.displayFormat = '%vX';
 		option.decimals = 2;
