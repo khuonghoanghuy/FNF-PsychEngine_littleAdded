7190e27c105fad2b1c176dad04684166d0694528
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-11-15 20:16:33 +0700

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
 import Controls.Control;
 import flixel.FlxG;
 import flixel.FlxSprite;
@@ -76,6 +78,12 @@ class PauseSubState extends MusicBeatSubstate
 		bg.alpha = 0;
 		bg.scrollFactor.set();
 		add(bg);
+
+		var grid:FlxBackdrop = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
+		grid.velocity.set(40, 40);
+		grid.alpha = 0;
+		FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
+		add(grid);
 
 		var levelInfo:FlxText = new FlxText(20, 15, 0, "", 32);
 		levelInfo.text += PlayState.SONG.song;
