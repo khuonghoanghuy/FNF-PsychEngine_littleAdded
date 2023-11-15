63c41ab404dae39e11874670ed37a9f31a3fc7db
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-11-15 20:15:36 +0700

    Update ModsMenuState.hx

--- source/ModsMenuState.hx
+++ source/ModsMenuState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
 #if desktop
 import Discord.DiscordClient;
 #end
@@ -70,6 +72,12 @@ class ModsMenuState extends MusicBeatState
 		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
 		WeekData.setDirectoryFromWeek();
+
+		var grid:FlxBackdrop = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
+		grid.velocity.set(40, 40);
+		grid.alpha = 0;
+		FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
+		add(grid);
 
 		#if desktop
 		// Updating Discord Rich Presence
